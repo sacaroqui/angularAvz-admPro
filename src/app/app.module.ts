fbc86c90b87d7ecd437fb643907e43bc7f44ba80
SamirCaro <sacaroqui@gmail.com>
2023-04-21 16:17:45 -0500

    Fin seccion 6 Inputs,Outputs, graficas rama-Input-Outputs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,10 +15,11 @@ import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
 
 
+
 @NgModule({
   declarations: [
     AppComponent,
-    NopagefoundComponent,
+    NopagefoundComponent
   ],
   imports: [
     BrowserModule,
